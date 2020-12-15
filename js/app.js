0ca610563108ab33d4e67b83af696a502f644afb
Joshua Swartzell <jswartzell1s@ctccode-c02d72vmm6kg.cape.k12.mo.us>
2020-12-15 14:35:26 -0600

    Added function to add active class by position

--- js/app.js
+++ js/app.js
@@ -13,14 +13,29 @@
  *
 */
 
+/* Global variables for  populating the menu */
+const navList = document.querySelector('.nav-links');
+const sectionNames = document.querySelectorAll('section');
+
+/* Global variables for finding if something is onscreen */
+const mySection = document.querySelector('section');
+const myRectangle = mySection.getBoundingClientRect();
+
 /* Function to return the screen to the top on refresh. Source: Stack Overflow */
 window.onbeforeunload = function () {
   window.scrollTo(0,0);
 };
 
-/* Global variables for  populating the menu */
-const navList = document.querySelector('.nav-links');
-const sectionNames = document.querySelectorAll('section');
+/* Helper function to determine if an element is in  the viewport */
+function isInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= window.innerHeight &&
+      rect.right <= window.innerWidth
+  );
+}
 
 /* Function I created to iterate through all sections and display their data-nav value as the link in the nav-bar */
 function buildNav() {
@@ -35,7 +50,12 @@ function buildNav() {
 buildNav();
 
 /* Adding the active-class to the focused section */
-
+document.addEventListener('scroll', function () {
+  const active = isInViewport(mySection) ?
+  mySection.classList.add('active-class') : mySection.classList.remove('active-class');
+}, {
+  passive: true
+})
 
 // Scroll to anchor ID using scrollTO event
 
