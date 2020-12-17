358a59d1fb4264510e569a767b876cde2d40caac
Joshua Swartzell <50595685+Joshuas193@users.noreply.github.com>
2020-12-16 21:53:27 -0600

    Function to make all sections higlight completed

--- js/app.js
+++ js/app.js
@@ -13,30 +13,16 @@
  *
 */
 
-/* Global variables for  populating the menu */
+/* Global variables */
 const navList = document.querySelector('.nav-links');
 const sectionNames = document.querySelectorAll('section');
+const links = document.querySelectorAll('.nav-links li');
 
-/* Global variables for finding if something is onscreen */
-const mySection = document.querySelector('section');
-const myRectangle = mySection.getBoundingClientRect();
 
 /* Function to return the screen to the top on refresh. Source: Stack Overflow */
 window.onbeforeunload = function () {
   window.scrollTo(0,0);
 };
-
-/* Helper function to determine if an element is in  the viewport */
-function isInViewport(el) {
-  const rect = el.getBoundingClientRect();
-  return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= window.innerHeight &&
-      rect.right <= window.innerWidth
-  );
-}
-
 /* Function I created to iterate through all sections and display their data-nav value as the link in the nav-bar */
 function buildNav() {
   for (const sectionName of sectionNames) {
@@ -49,17 +35,42 @@ function buildNav() {
 /* Calling the function I built */
 buildNav();
 
-/* Adding the active-class to the focused section */
+// Helper function to determine if an element is in  the viewport
+function isInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+      rect.top >= -125 &&
+      rect.left >= 0 &&
+      rect.bottom <= 1000 &&
+      rect.right <= window.innerWidth
+  );
+}
 
-document.addEventListener('scroll', function () {
-  const active = isInViewport(mySection) ?
-  mySection.classList.add('active-class') : mySection.classList.remove('active-class');
-}, {
-  passive: true
-})
+//  Adding the active-class to the focused section
+for (const sectionName of sectionNames) {
+  document.addEventListener('scroll', function () {
+    const active = isInViewport(sectionName) ? sectionName.classList.add('active-class') :
+      sectionName.classList.remove('active-class');
+  }, {
+    passive: true
+  });
+}
 
 // Scroll to anchor ID using scrollTO event
+for (const link of links) {
+  console.log(link.textContent);
+  link.addEventListener('click', clickHandler);
+}
 
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offset = document.querySelector(href).offsetTop;
+  scroll({
+    top: offset,
+    behavior: "smooth"
+  });
+}
 
 /**
  * End Main Functions
