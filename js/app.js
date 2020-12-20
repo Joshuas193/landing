d2c6ca352e6dc96ef6fa9e28e437b54deaa313b8
Joshua Swartzell <50595685+Joshuas193@users.noreply.github.com>
2020-12-19 22:25:17 -0600

    refactored some functions and organized some code

--- js/app.js
+++ js/app.js
@@ -13,75 +13,73 @@
  *
 */
 
-/* Global variables */
+// Global variables
 const navList = document.querySelector('.nav-links');
 const sectionNames = document.querySelectorAll('section');
 
-/* Function to return the screen to the top on refresh. Source: Stack Overflow */
+// Function to return the screen to the top on refresh. Source: Stack Overflow
 window.onbeforeunload = function () {
   window.scrollTo(0,0);
 };
-/* Function I created to iterate through all sections and display their data-nav value as the link in the nav-bar */
-function buildNav() {
-  for (const sectionName of sectionNames) {
-    const list = document.createElement('li');
-    const addedHtml = `<a href="#${sectionName.id}">${sectionName.dataset.nav}</a>`;
-    navList.appendChild(list);
-    list.insertAdjacentHTML('afterbegin', addedHtml);
-  }
-}
-/* Calling the function I built */
-buildNav();
 
-// Helper function to determine if an element is in  the viewport
+// Helper function to determine if an element is in the viewport
 function isInViewport(el) {
   const rect = el.getBoundingClientRect();
   return (
       rect.top >= -100 &&
       rect.top < 100 &&
       rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight - 50) &&
+      rect.bottom <= (window.innerHeight + 100) &&
       rect.right <= window.innerWidth
   );
 }
 
-//  Adding the active-class to the focused section
-for (const sectionName of sectionNames) {
-  document.addEventListener('scroll', function () {
-    const active = isInViewport(sectionName) ? sectionName.classList.add('active-class') :
-      sectionName.classList.remove('active-class');
-  }, {
-    passive: true
-  });
+// Helper function Creating a disapearing NavBar effect
+function timeOut() {
+  const navbar = document.querySelector('.nav-header');
+  navbar.style.top = '-200px';
 }
 
-// declaring variable for the links listener after the  navbar is populated
-const links = document.querySelectorAll('.nav-links li a');
-
-// Scroll to anchor ID using scrollTO event
-for (const link of links) {
-  link.addEventListener('click', clickHandler);
-}
-
+// Helper function creating Smooth scrolling and offset top features
 function clickHandler(e) {
   e.preventDefault();
   const href = this.getAttribute("href");
   const offset = document.querySelector(href).offsetTop;
-  console.log(offset);
   scroll({
     top: (offset - 50),
     behavior: "smooth"
   });
+  setTimeout(timeOut, 1500);
+  document.getElementById('up-top').style.display = 'block';
 }
 
-/**
- * End Main Functions
- * Begin Events
- *
-*/
+// Function I created to iterate through all sections and display their data-nav value as the link in the nav-bar
+const buildNav = function() {
+  for (const sectionName of sectionNames) {
+    const list = document.createElement('li');
+    const addedHtml = `<a href="#${sectionName.id}">${sectionName.dataset.nav}</a>`;
+    navList.appendChild(list);
+    list.insertAdjacentHTML('afterbegin', addedHtml);
+  }
+};
+buildNav();
 
-// Build menu
+//  Adding the active-class to the focused section
+const viewedSection = function () {
+  for (const sectionName of sectionNames) {
+    document.addEventListener('scroll', function () {
+      const active = isInViewport(sectionName) ? sectionName.classList.add('active-class') :
+        sectionName.classList.remove('active-class');
+      }, {
+    passive: true
+  });
+}};
+viewedSection();
 
-// Scroll to section on link click
-
-// Set sections as active
+// declaring function for the links event listener after the navbar is populated
+const linkListener = function() {
+  const links = document.querySelectorAll('.nav-links li a');
+  for (const link of links) {
+    link.addEventListener('click', clickHandler);
+  }};
+linkListener();
