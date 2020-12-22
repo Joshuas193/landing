5848a935094cc302e4be905d72e02eabeef81e60
Joshua Swartzell <50595685+Joshuas193@users.noreply.github.com>
2020-12-22 14:02:29 -0600

    Finishing up some JS additions and style changes

--- js/app.js
+++ js/app.js
@@ -22,8 +22,20 @@ window.onbeforeunload = function () {
   window.scrollTo(0,0);
 };
 
+/* This function will hide the navbar on scroll down and show it on scroll up.
+   It also shows the back to top button if scrolled down far enough */
+let prevPosition = window.pageYOffset;
+window.onscroll = () => {
+  let newPosition = window.pageYOffset;
+    (prevPosition > newPosition) ? document.querySelector('.nav-header').style.top = '0' :
+                                   document.querySelector('.nav-header').style.top = '-200px';
+      prevPosition = newPosition;
+    (newPosition > 1500) ? document.getElementById('up-top').style.display = 'block' :
+                           document.getElementById('up-top').style.display = 'none';
+};
+
 // Helper function to determine if an element is in the viewport
-function isInViewport(el) {
+isInViewport = el => {
   const rect = el.getBoundingClientRect();
   return (
       rect.top >= -100 &&
@@ -35,9 +47,8 @@ function isInViewport(el) {
 }
 
 // Helper function Creating a disapearing NavBar effect
-function timeOut() {
-  const navbar = document.querySelector('.nav-header');
-  navbar.style.top = '-200px';
+timeOut = () => {
+  document.querySelector('.nav-header').style.top = '-200px';
 }
 
 // Helper function creating Smooth scrolling and offset top features
@@ -47,14 +58,13 @@ function clickHandler(e) {
   const offset = document.querySelector(href).offsetTop;
   scroll({
     top: (offset - 50),
-    behavior: "smooth"
+    behavior: 'smooth'
   });
-  setTimeout(timeOut, 1500);
-  document.getElementById('up-top').style.display = 'block';
+  setTimeout(timeOut, 1200);
 }
 
 // Function I created to iterate through all sections and display their data-nav value as the link in the nav-bar
-const buildNav = function() {
+buildNav = () => {
   for (const sectionName of sectionNames) {
     const list = document.createElement('li');
     const addedHtml = `<a href="#${sectionName.id}">${sectionName.dataset.nav}</a>`;
@@ -64,11 +74,19 @@ const buildNav = function() {
 };
 buildNav();
 
-//  Adding the active-class to the focused section
-const viewedSection = function () {
+// function for the links event listener after the navbar is populated
+linkListener = () => {
+  const links = document.querySelectorAll('.nav-links li a');
+  for (const link of links) {
+    link.addEventListener('click', clickHandler);
+  }};
+linkListener();
+
+//  Adding the active-class to the in-view section
+viewedSection = () => {
   for (const sectionName of sectionNames) {
-    document.addEventListener('scroll', function () {
-      const active = isInViewport(sectionName) ? sectionName.classList.add('active-class') :
+    document.addEventListener('scroll', () => {
+      isInViewport(sectionName) ? sectionName.classList.add('active-class') :
         sectionName.classList.remove('active-class');
       }, {
     passive: true
@@ -76,10 +94,17 @@ const viewedSection = function () {
 }};
 viewedSection();
 
-// declaring function for the links event listener after the navbar is populated
-const linkListener = function() {
-  const links = document.querySelectorAll('.nav-links li a');
-  for (const link of links) {
-    link.addEventListener('click', clickHandler);
-  }};
-linkListener();
+// This function makes the nav-bar visible again when back to top clicked
+backToTop = () => {
+  const upTop = document.getElementById('up-top');
+  upTop.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.getElementById('up-top').style.display = 'none';
+    document.querySelector('.nav-header').style.top = '0';
+    scroll({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+};
+backToTop();
